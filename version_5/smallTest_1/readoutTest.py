75196053a8306003da31af27d5f706ea64f8ee6d
Adam Frees <adamfrees@Adams-iMac.local>
2016-04-15 21:38:56 -0500

    Autotune now works on small device. Dot occupation is read through COMSOL surface integration (added to comsol-python repo).

--- version_5/smallTest_1/readoutTest.py
+++ version_5/smallTest_1/readoutTest.py
@@ -16,17 +16,22 @@ autoTuneIteration = 0
 
 modelFile = '/Users/adamfrees/Dropbox/_UW/Scale-up/Comsol_test_1/simpleSingleDot.mph'
 
-target = [2.]
+target = [1.]
 
-allHeightsStartingParams=[[30.,[0.190050340468617,-0.2,-0.2],'L1'],[32.,[0.20027523864318902,-0.2,-0.2],'L1'],[34.,[0.21058880122396614,-0.2,-0.2],'L1'],[36.,[0.221065369580485,-0.2,-0.2],'L1'],[38.,[0.23163401295215844,-0.2,-0.2],'L1']]
+allHeightsStartingParams=[[30.,[0.2,-0.2,-0.2],'L1'],[32.,[0.2,-0.2,-0.2],'L1'],[34.,[0.2,-0.2,-0.2],'L1'],[36.,[0.2,-0.2,-0.2],'L1'],[38.,[0.2,-0.2,-0.2],'L1']]
 
 order=range(1,4)
 
 
 if useCOMSOL:
-    dDot = model(comsolBinLocation='/Applications/COMSOL52/Multiphysics/bin/')
+    dDot = model()
     dDot.loadModelFile(modelFile)
+dDot.runSimulation('std1')
 
+print dDot.evaluateNumerical('int1')
+
+
+'''
 #params = allHeightsStartingParams[0]
 for params in allHeightsStartingParams:
     autoTuneRunning = True
@@ -34,7 +39,7 @@ for params in allHeightsStartingParams:
     #######################################
     method = params[2]
     height = params[0]
-    parentFolder = '/Users/adamfrees/Dropbox/_UW/CODA/version_5/SmallTest1/'+("%.0f" % height)+'nm'
+    parentFolder = '/home/frees/Dropbox/_UW/CODA/version_5/SmallTest1/'+("%.0f" % height)+'nm'
     if not os.path.exists(parentFolder):
             os.makedirs(parentFolder)
 
@@ -85,21 +90,29 @@ for params in allHeightsStartingParams:
 
             startTime = time()
             dDot.runSimulation('std1')
-            densities = [[dDot.evaluateNumerical('int1')]]
+
             #dDot.exportData(dataFolder+'/Run_'+str(runNum)+'.txt','data1')
             runNum +=1
             print "First run: ", time()-startTime
 
             gateList = []
             paramList = []
-            voltageStepSize = 1.e-4
-            startTime = time()
             for gateNum in order:
-                newVolt=dDot.getParam('volt'+str(gateNum))+voltageStepSize
-                dDot.setParam('volt'+str(gateNum),newVolt)
-                dDot.runSimulation('std1')
-                densities += [[dDot.evaluateNumerical('int1')]]
-                dDot.setParam('volt'+str(gateNum),newVolt-voltageStepSize)
+                paramList += [dDot.getParam('volt'+str(gateNum))]
+                gateList += ['volt'+str(gateNum)]
+            paramMat = array([paramList]*len(order)).transpose()
+
+            voltageStepSize = 1.e-4
+
+            paramMat += voltageStepSize*eye(len(order))
+
+            dDot.initializeSweep(gateList,paramMat)
+
+            startTime = time()
+            dDot.runSimulation('std2')
+
+            for i in range(len(order)):
+                #dDot.exportData(dataFolder+'/Run_'+str(runNum)+'.txt','data3',i+1)
                 runNum +=1
 
             print "Subsequent "+str(len(order))+" runs: ",time()-startTime
@@ -112,11 +125,11 @@ for params in allHeightsStartingParams:
             outputFile = open(codaDataFolder+'/CODA_data.txt','w')
 
             for i in range(len(order)+1):
-                density = array(densities[i])
+                density,ts = analyzeDataFile(dataFolder+'/Run_'+str(i)+'.txt')
                 outputVolts = currentVolts
                 if i>0:
                     outputVolts[i-1] += voltageStepSize
-                outputFile.write(str(outputVolts+density.tolist()).replace(',','').replace(']','').replace('[','')+' \n')
+                outputFile.write(str(outputVolts+density.tolist()+ ts.tolist()).replace(',','').replace(']','').replace('[','')+' \n')
                 if i>0:
                     outputVolts[i-1] -= voltageStepSize
             outputFile.close()
@@ -141,9 +154,9 @@ for params in allHeightsStartingParams:
             print "Run number "+str(runNum)+": ", time()-startTime
 
             #density,ts = analyzeDataFile(dataFolder+'/Run_'+str(runNum)+'.txt')
-            density=[dDot.evaluateNumerical('int1')]
+
             predictedDeps = predictDeps(codaDataFolder+'/CODA_data.txt',alpha*voltageChanges)
-            differenceFromLinear = [abs(density[0]-predictedDeps[0])]
+            differenceFromLinear = abs(density[0]-predictedDeps[0]).tolist()
             #differenceFromLinear += (0.5*abs(array(map(log,ts))-predictedDeps[2:])).tolist()
             differenceFromLinearNorm = norm(array(differenceFromLinear),ord=2)
             normalizedDistance = differenceFromLinearNorm/totalDistance
@@ -151,12 +164,12 @@ for params in allHeightsStartingParams:
                 errorCounter += 1
             else:
                 errorCounter = 0
-                optimalSystemParams = [density]
+                optimalSystemParams = [density,ts]
                 optimalVoltageChanges = alpha*array(voltageChanges)
                 convergenceData[0] += [alpha*solutionL2Norm]
                 convergenceData[1] += [normalizedDistance]
-                attemptDistance = [abs(density[0]-target[0])]
-                #attemptDistance += (0.5*abs(array(map(log,ts))-array(map(log,target[4:])))).tolist()
+                attemptDistance = abs(density[0:2]-target[0:2]).tolist()
+                attemptDistance += (0.5*abs(array(map(log,ts))-array(map(log,target[4:])))).tolist()
                 attemptDistanceNorm = norm(array(attemptDistance),ord=2)
             if errorCounter>1:
                 break
@@ -188,10 +201,10 @@ for params in allHeightsStartingParams:
         autoTuneIteration += 1
 
 
-        if attemptDistanceNorm < 0.01:
+        if attemptDistanceNorm < 0.05:
             print 'Finished AutoTune!'
             print 'The voltages:'
             print currentVolts
             print 'yield the system state:'
             print optimalSystemParams
-            autoTuneRunning = False
+            autoTuneRunning = False'''
